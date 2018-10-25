270459bf0dbf2e3529fbcbe7121cc9cb48f0776c
XiongPlus <251891863@qq.com>
2018-10-25 23:10:13 +0800

    淡ç单人页面完完成

---
+++ miniprogram/components/header-part/header-part.js
@@ -0,0 +1,30 @@
+// components/header-part/header-part.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    text: {
+      type: String,
+      value: ''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    toXishiduo(){
+      wx.navigateTo({
+        url: '../',
+      })
+    }
+  }
+})
