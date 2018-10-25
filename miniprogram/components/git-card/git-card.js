f1b2416f717aaf0cd957f7da419e478f6e66d3ff
Brynhild <402904972@qq.com>
2018-10-25 21:46:12 +0800

    增加卡片组件和布局

---
+++ miniprogram/components/git-card/git-card.js
@@ -0,0 +1,27 @@
+// components/git-card/git-card.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    imgUrl:null,
+    sum:null,
+    special:null
+  },
+
+
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
